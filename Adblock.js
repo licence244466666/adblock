e245adaef32a15a484345b896c1c6b2a2271c070
licence244466666 <parasharivip@gmail.com>
2022-07-11 12:56:05 +0530

    Update Adblock.js

--- Adblock.js
+++ Adblock.js
@@ -1,7 +1,8 @@
-    !function() {
+   !function() {
       function f() {
         var a = document.createElement("div"); a.id = "arlinablock"; a.innerHTML = '<div class="inner"> <div class="header"> <h2>Ad Blocker Detected</h2> </div> <div class="isi"> <p>Please consider supporting us by disabling your ad blocker</p> <div class="tombol"><button class="1 active">Adblock</button></div> </div> </div> </div>';
-        var b = document.createElement("script"); b.type = "text/javascript";
+        
+          b[c].classList.remove("active"); b[a-1].classList.add("active"); d[a-1].classList.add("active")})}var b = document.createElement("script"); b.type = "text/javascript";
       b.async=!0; b.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"; b.onerror = function() {
         f(); window.adblock=!0
       }; var e = document.getElementsByTagName("script")[0]; e.parentNode.insertBefore(b,
